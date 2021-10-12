a1ccc48eb5c2bd319bbf37ec15cf8df64c4f7133
Paul Marolop <paulriomarolop@gmail.com>
2021-10-12 22:46:39 +0700

    Paul Rio Marolop add layout in home fragment connect api for search lyric (beta)

--- app/src/main/java/com/example/ambilv2/MainActivity.java
+++ app/src/main/java/com/example/ambilv2/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.ambilv2;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +22,12 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.ambilv2.databinding.ActivityMainBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
     private ActivityMainBinding binding;
 
     @Override
@@ -22,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        textView = findViewById(R.id.hasilPencarian);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -32,6 +47,44 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
+
+
+    public void searchLyric(View view) {
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://api.lyrics.ovh/v1/coldplay/yellow";
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url,null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        // Display the first 500 characters of the response string.
+
+                        try {
+                            //JSONObject jsonobject = response.getJSONObject("lyrics");
+                           // Log.i("TAG", "onResponse: "+ jsonobject);
+                            textView.setText(response.getString("lyrics"));
+                        } catch (JSONException e) {
+                           e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+
 
 }
