4b27fcf09d35875743c58a22e55e1ef33b360768
Paul Marolop <paulriomarolop@gmail.com>
2021-11-12 00:30:48 +0700

    Add Feature User can search lyric by input artist name and song title (Paul Rio Marolop)

--- app/src/main/java/com/example/ambilv2/MainActivity.java
+++ app/src/main/java/com/example/ambilv2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ambilv2;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -29,11 +31,15 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private ActivityMainBinding binding;
+    private EditText mArtistName;
+    private EditText mSongTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mArtistName = findViewById(R.id.artis_name);
+        mSongTitle = findViewById(R.id.song_title_label);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         textView = findViewById(R.id.hasilPencarian);
@@ -48,41 +54,43 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
+
+
     }
 
 
-    public void searchLyric(View view) {
-
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://api.lyrics.ovh/v1/coldplay/yellow";
-
-        // Request a string response from the provided URL.
-        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url,null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-
-                        // Display the first 500 characters of the response string.
-
-                        try {
-                            //JSONObject jsonobject = response.getJSONObject("lyrics");
-                           // Log.i("TAG", "onResponse: "+ jsonobject);
-                            textView.setText(response.getString("lyrics"));
-                        } catch (JSONException e) {
-                           e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
-        });
-
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
-    }
+//    public void searchLyric(View view) {
+//        Log.i("Nama Artist", "searchLyric: " + mArtistName.getText().toString());
+//        // Instantiate the RequestQueue.
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//        String url ="https://api.lyrics.ovh/v1/" + mArtistName.getText().toString() + "/" + mSongTitle.getText().toString();
+//
+//        // Request a string response from the provided URL.
+//        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url,null,
+//                new Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//
+//                        // Display the first 500 characters of the response string.
+//
+//                        try {
+//                            //JSONObject jsonobject = response.getJSONObject("lyrics");
+//                           // Log.i("TAG", "onResponse: "+ jsonobject);
+//                            textView.setText(response.getString("lyrics"));
+//                        } catch (JSONException e) {
+//                           e.printStackTrace();
+//                        }
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                textView.setText("That didn't work!");
+//            }
+//        });
+//
+//// Add the request to the RequestQueue.
+//        queue.add(stringRequest);
+//    }
 
 
 
